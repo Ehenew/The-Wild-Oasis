39a09085af47f8e1ee7dcfd273edf56eb6edc766
Ehenew <ehenew16@gmail.com>
2025-04-07 15:47:11 +0300

    ui leak fix

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -2,8 +2,6 @@ import UpdateSettingsForm from "../features/settings/UpdateSettingsForm";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
-
-
 function Settings() {
   return (
     <Row>
